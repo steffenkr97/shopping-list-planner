ae5f9d7b8dae1ece047338a3d3bb4a6a95a34443
Steffen Kroll <kroll.steffen@icloud.com>
2025-03-16 16:51:18 +0100

    style

--- static/script.js
+++ static/script.js
@@ -1,19 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    loadCategories();
+
+    document.getElementById("category-form").addEventListener("submit", async function (event) {
+        event.preventDefault();
+        const categoryName = document.getElementById("category-name").value;
+
+        const response = await fetch("/api/category", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name: categoryName})
+        });
+
+        if (response.ok) {
+            loadCategories();
+            this.reset();
+        } else {
+            alert("Fehler beim Hinzufügen der Kategorie!");
+        }
+    });
+
+});
+
 async function loadCategories() {
     const response = await fetch("/api/category/all");
     const categories = await response.json();
     const categoryList = document.getElementById("category-list");
-
     categoryList.innerHTML = "";
+
     categories.forEach(category => {
         const li = document.createElement("li");
         li.textContent = category.name;
         categoryList.appendChild(li);
-    })
+    });
 }
-
-
-function addCategory() {
-    
-}
-
-loadCategories()
